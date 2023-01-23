85f6e666616fdef0edba477c2024f35b76208504
ClainTxt <123306093+ClainTxt@users.noreply.github.com>
2023-01-23 19:37:37 +0400

    Add files via upload

--- clain_imgui.lua
+++ clain_imgui.lua
@@ -13,6 +13,11 @@ ffi = require 'ffi'
 imgui = require "imgui"
 str = ffi.string
 fa = require "fAwesome5"
+local rkeys = require 'rkeys'
+imgui.ToggleButton = require('imgui_addons').ToggleButton
+imgui.HotKey = require('imgui_addons').HotKey
+imgui.Spinner = require('imgui_addons').Spinner
+imgui.BufferingBar = require('imgui_addons').BufferingBar
 --
 -- К основным+
 local bNotf, notf = pcall(import, "imgui_notf.lua")
@@ -21,6 +26,7 @@ local fa_glyph_ranges = imgui.ImGlyphRanges({ fa.min_range, fa.max_range })
 --
 -- Булевые переменные
 status = false
+auto_send = imgui.ImBool(false)
 --
 -- Конфиг переменные
 directIni = 'nvsettings'
@@ -28,26 +34,26 @@ config = inicfg.load(nil, directIni)
 -- Update system
 update_state = false
 
-local script_vers = 2 -- не менять
-local script_vers_text = "1.2" -- не менять
+local script_vers = 3 -- не менять
+local script_vers_text = "1.3" -- не менять
 
 local update_url = "https://raw.githubusercontent.com/ClainTxt/scripts/main/update.ini" -- не менять
 local update_path = getWorkingDirectory() .. "/update.ini" -- не менять
-
 local script_url = "https://raw.githubusercontent.com/ClainTxt/scripts/main/clain_imgui.lua" -- не менять
 local script_path = thisScript().path -- не менять
-
 --
 -- Имгуи переменные
 nvstatus = imgui.ImBool(config.settings.nvstatus)
 adsstatus = imgui.ImBool(config.settings.adsstatus)
 askstatus = imgui.ImBool(config.settings.askstatus)
 text_buffer = imgui.ImBuffer(config.hide.nick, 256)
+text_auto = imgui.ImBuffer(config.auto.text, 256)
+auto_wait = imgui.ImInt(config.auto.wait)
 gchatstatus = imgui.ImBool(config.settings.gchatstatus)
 themeselected = imgui.ImInt(config.settings.themeselected)
 checkanim = config.settings.checkanim
 main_window_state = imgui.ImBool(false)
-local combo_dada = imgui.ImInt(0)
+combo_dada = imgui.ImInt(0)
 --
 local themes = import "resource/imgui_themes.lua"
 -- 
@@ -136,6 +142,39 @@ function main() -- Главная функция сампа
 		end
 	end
 end
+function imgui.TextQuestion(label, description)
+    imgui.TextDisabled(label)
+
+    if imgui.IsItemHovered() then
+        imgui.BeginTooltip()
+            imgui.PushTextWrapPos(600)
+                imgui.TextUnformatted(description)
+            imgui.PopTextWrapPos()
+        imgui.EndTooltip()
+    end
+end
+function imgui.CenterText(text)
+    local width = imgui.GetWindowWidth()
+    local calc = imgui.CalcTextSize(text)
+    imgui.SetCursorPosX( width / 2 - calc.x / 2 )
+    imgui.Text(text)
+end
+function imgui.NewInputText(lable, val, width, hint, hintpos)
+    local hint = hint and hint or ''
+    local hintpos = tonumber(hintpos) and tonumber(hintpos) or 1
+    local cPos = imgui.GetCursorPos()
+    imgui.PushItemWidth(width)
+    local result = imgui.InputText(lable, val)
+    if #val.v == 0 then
+        local hintSize = imgui.CalcTextSize(hint)
+        if hintpos == 2 then imgui.SameLine(cPos.x + (width - hintSize.x) / 2)
+        elseif hintpos == 3 then imgui.SameLine(cPos.x + (width - hintSize.x - 5))
+        else imgui.SameLine(cPos.x + 5) end
+        imgui.TextColored(imgui.ImVec4(1.00, 1.00, 1.00, 0.40), tostring(hint))
+    end
+    imgui.PopItemWidth()
+    return result
+end
 function imgui.BeforeDrawFrame() -- Подключаем иконки
     if fa_font == nil then
         local font_config = imgui.ImFontConfig() -- to use 'imgui.ImFontConfig.new()' on error
@@ -147,21 +186,23 @@ function imgui.OnDrawFrame() -- Когда имгуи запущен
     local sw, sy = getScreenResolution() -- Позиция экрана
     if not main_window_state.v then imgui.Process = false end
 	if main_window_state.v then
-		imgui.SetNextWindowSize(imgui.ImVec2(500, 300), imgui.Cond.FirstUseEver) 
+		imgui.SetNextWindowSize(imgui.ImVec2(500, 200), imgui.Cond.FirstUseEver) 
         imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sy / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
 		imgui.Begin(u8" .SIXTEEN Настройки скрипта", main_window_state, imgui.WindowFlags.NoResize+imgui.WindowFlags.NoCollapse )
-			imgui.PushItemWidth(60)
+			imgui.BeginChild('123', imgui.ImVec2(250, 165), true)
+			imgui.PushItemWidth(95)
 			imgui.InputText(u8'Ник кого скрыть', text_buffer)
 			imgui.SameLine()
-			if imgui.Button(''..fa.ICON_FA_USER_PLUS, imgui.ImVec2(40, 20)) then
+			if imgui.Button(fa.ICON_FA_USER_PLUS .. '##1', imgui.ImVec2(40, 20)) then
 				if config.hide.nick ~= text_buffer.v then 
 					config.hide.nick = text_buffer.v
 					if inicfg.save(config, directIni) then msgChat("Ник сохранен") end
 				else msgChat("Этот ник и так в базе") end
 			end
-			imgui.Separator()
-			imgui.Checkbox(fa.ICON_FA_COMMENT .. u8' Прием сообщений', nvstatus)
-			if nvstatus then 
+			imgui.ToggleButton(fa.ICON_FA_COMMENT .. u8' Прием сообщений', nvstatus)
+			imgui.SameLine()
+			imgui.TextQuestion('( ? )',u8"Когда чел пишет =[text], Ты отправишь [text] в чат")
+			if nvstatus.v then 
 				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ads', adsstatus)
 				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ask', askstatus)
 				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на !', gchatstatus)	
@@ -173,23 +214,53 @@ function imgui.OnDrawFrame() -- Когда имгуи запущен
 				config.settings.gchatstatus = gchatstatus.v
 				if inicfg.save(config, directIni) then msgChat("Настройки успешно сохранены") end
 			end
-			imgui.SetCursorPos(imgui.ImVec2(290, 60))
-			imgui.BeginChild(u8'Auto-Отправка', imgui.ImVec2(200, 123), true)
-				local x,y,z = getCharCoordinates(PLAYER_PED)
-				imgui.SetCursorPosX(5)
-				imgui.Text(u8"Координаты: " .. math.floor(x) .. " " .. math.floor(y) .. " " .. math.floor(z))
-				imgui.Combo(u8'Темы', combo_dada, themes.colorThemes, #themes.colorThemes) 
-				if imgui.Button(fa.ICON_FA_PLAY .. "", imgui.ImVec2(50, 20)) then
-					themes.SwitchColorTheme(combo_dada.v +1)
-					config.settings.themeselected = combo_dada.v+1
-					if inicfg.save(config, directIni) then 
-						if bNotf then
-							notf.addNotification("Тема изменена! ", 3, 1)
-						end 
-					end
+			local x,y,z = getCharCoordinates(PLAYER_PED)
+			--imgui.Spinner(15,5)
+			imgui.PushItemWidth(120)
+			imgui.Combo(u8'', combo_dada, themes.colorThemes, #themes.colorThemes)
+			imgui.SameLine() 
+			if imgui.Button(fa.ICON_FA_PLAY .. "", imgui.ImVec2(50, 20)) then
+				themes.SwitchColorTheme(combo_dada.v +1)
+				config.settings.themeselected = combo_dada.v+1
+				if inicfg.save(config, directIni) then 
+					if bNotf then
+						notf.addNotification("Тема изменена! ", 3, 1)
+					end 
 				end
+			end
+			imgui.Text(u8"Координаты: " .. math.floor(x) .. " " .. math.floor(y) .. " " .. math.floor(z))
+			imgui.EndChild()
+			imgui.SameLine()
+			imgui.BeginChild(u8'Auto-Отправка', imgui.ImVec2(235, 165), true)
+			imgui.CenterText(u8"Настройки авто-отправки")
+			imgui.InputInt(u8'кд/сек', auto_wait)
+			imgui.SameLine()
+			imgui.TextQuestion('( ? )',u8"Время задержки в секундах")
+			imgui.NewInputText('##autotext', text_auto, 165, u8"Текст авто-отправки", 2)
+			imgui.SameLine()
+			if imgui.Button(fa.ICON_FA_PLAY .. '##1', imgui.ImVec2(20, 20)) then
+				auto_send.v = not auto_send.v
+				if auto_send.v then
+					config.auto.text = u8:decode(text_auto.v)
+					config.auto.wait = auto_wait.v 
+					inicfg.save(config, directIni)
+					msgChat("Авто-Текст запущен")
+					msgChat("Задержка: ".. config.auto.wait)
+				elseif not auto_send.v then
+					msgChat("Авто-Текст завершен")
+				end
+				lua_thread.create(autosendfunc)
+			end
 			imgui.EndChild()
 		imgui.End()	
+	end
+end
+function autosendfunc()
+	if auto_send.v then
+		while auto_send.v do
+			wait(config.auto.wait*1000)
+			sampSendChat(config.auto.text)
+		end 
 	end
 end
 function checkanim()
@@ -232,22 +303,30 @@ function sampev.onServerMessage(color, text)
 	if nvstatus.v then 
 		if text:find("(.*) говорит: =(.*)") then
 			local name, msg = text:match("(.*) говорит: =(.*)")
-			if msg:find("!") and gchatstatus.v then
+			if msg:find("^!") and gchatstatus.v then
 				sampSendChat(msg)
 				return true
-			elseif msg:find("/ads") and adsstatus.v then
+			elseif msg:find("^/ads") and adsstatus.v then
 				sampSendChat(msg)
 				return true
-			elseif msg:find("/ask") and askstatus.v then
+			elseif msg:find("^/ask") and askstatus.v then
 				sampSendChat(msg)
 				return true
 			else
-				if msg:find("!") and not gchatstatus.v then return true
-				elseif msg:find("/ads") and not adsstatus.v then return true
-				elseif msg:find("/ask") and not askstatus.v then return true 
-				else msgChat(name .. " говорит: " .. msg) sampSendChat(msg) end
+				if msg:find("^!") and not gchatstatus.v then return true
+				elseif msg:find("^/ads") and not adsstatus.v then return true
+				elseif msg:find("^/ask") and not askstatus.v then return true 
+				else msgChat(name .. " говорит: " .. msg) sampSendChat(msg) return true end
 			end
 		end 
+	end
+end
+function sampev.onSendCommand(text)
+	if text == "/savepos" then
+	local x,y,z = getCharCoordinates(PLAYER_PED)
+	setClipboardText(("%d %d %d"):format(math.floor(x),math.floor(y),math.floor(z)))
+	msgChat("Корды скопированы")
+	return { text }
 	end
 end
 function laa()
