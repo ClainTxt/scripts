1d807f164a1a29472a81e44159648efea2c7d079
ClainTxt <123306093+ClainTxt@users.noreply.github.com>
2023-01-23 21:05:50 +0400

    Add files via upload

--- clain_imgui.lua
+++ clain_imgui.lua
@@ -30,12 +30,31 @@ auto_send = imgui.ImBool(false)
 --
 -- Конфиг переменные
 directIni = 'nvsettings'
-config = inicfg.load(nil, directIni)
+config = inicfg.load({
+	hide = {
+		nick = "test",
+	},
+	auto = {
+		wait = 5,
+		text = "test"
+	},
+	settings = {
+		adsstatus=false,
+		themeselected=9,
+		checkanim=false,
+		askstatus=false,
+		getstatus=false,
+		nvstatus=false,
+		gchatstatus=false,
+		password=33009911,
+		auto_password = false
+	}
+}, directIni)
 -- Update system
 update_state = false
 
-local script_vers = 3 -- не менять
-local script_vers_text = "1.3" -- не менять
+local script_vers = 4 -- не менять
+local script_vers_text = "1.4" -- не менять
 
 local update_url = "https://raw.githubusercontent.com/ClainTxt/scripts/main/update.ini" -- не менять
 local update_path = getWorkingDirectory() .. "/update.ini" -- не менять
@@ -47,13 +66,15 @@ nvstatus = imgui.ImBool(config.settings.nvstatus)
 adsstatus = imgui.ImBool(config.settings.adsstatus)
 askstatus = imgui.ImBool(config.settings.askstatus)
 text_buffer = imgui.ImBuffer(config.hide.nick, 256)
-text_auto = imgui.ImBuffer(config.auto.text, 256)
+text_auto = imgui.ImBuffer(tostring(config.auto.text), 256)
 auto_wait = imgui.ImInt(config.auto.wait)
 gchatstatus = imgui.ImBool(config.settings.gchatstatus)
 themeselected = imgui.ImInt(config.settings.themeselected)
 checkanim = config.settings.checkanim
 main_window_state = imgui.ImBool(false)
 combo_dada = imgui.ImInt(0)
+password = imgui.ImBuffer(tostring(config.settings.password), 256)
+auto_password = imgui.ImBool(config.settings.auto_password)
 --
 local themes = import "resource/imgui_themes.lua"
 -- 
@@ -182,6 +203,87 @@ function imgui.BeforeDrawFrame() -- Подключаем иконки
         fa_font = imgui.GetIO().Fonts:AddFontFromFileTTF('moonloader/lib/fa-solid-900.ttf', 13.0, font_config, fa_glyph_ranges)
     end
 end
+function FirstChild()
+	imgui.BeginChild('123', imgui.ImVec2(250, 165), true)
+	imgui.PushItemWidth(95)
+	imgui.InputText(u8'Ник кого скрыть', text_buffer)
+	imgui.SameLine()
+	if imgui.Button(fa.ICON_FA_USER_PLUS .. '##1', imgui.ImVec2(40, 20)) then
+		if config.hide.nick ~= text_buffer.v then 
+			config.hide.nick = text_buffer.v
+			if inicfg.save(config, directIni) then msgChat("Ник сохранен") end
+		else msgChat("Этот ник и так в базе") end
+	end
+	imgui.ToggleButton(fa.ICON_FA_COMMENT .. u8' Прием сообщений', nvstatus)
+	imgui.SameLine()
+	imgui.TextQuestion('( ? )',u8"Когда чел пишет =[text], Ты отправишь [text] в чат")
+	if nvstatus.v then 
+		imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ads', adsstatus)
+		imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ask', askstatus)
+		imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на !', gchatstatus)	
+	end
+	if imgui.Button(fa.ICON_FA_CLOUD_DOWNLOAD_ALT .. u8' Сохранить настройки', imgui.ImVec2(155, 30)) then
+		config.settings.nvstatus = nvstatus.v
+		config.settings.adsstatus = adsstatus.v
+		config.settings.askstatus = askstatus.v
+		config.settings.gchatstatus = gchatstatus.v
+		if inicfg.save(config, directIni) then msgChat("Настройки успешно сохранены") end
+	end
+	local x,y,z = getCharCoordinates(PLAYER_PED)
+	--imgui.Spinner(15,5)
+	imgui.PushItemWidth(120)
+	imgui.Combo(u8'', combo_dada, themes.colorThemes, #themes.colorThemes)
+	imgui.SameLine() 
+	if imgui.Button(fa.ICON_FA_PLAY .. "", imgui.ImVec2(50, 20)) then
+		themes.SwitchColorTheme(combo_dada.v +1)
+		config.settings.themeselected = combo_dada.v+1
+		if inicfg.save(config, directIni) then 
+			if bNotf then
+				notf.addNotification("Тема изменена! ", 3, 1)
+			end 
+		end
+	end
+	imgui.Text(u8"Координаты: " .. math.floor(x) .. " " .. math.floor(y) .. " " .. math.floor(z))
+	imgui.EndChild()
+end
+function SecondChild()
+	imgui.BeginChild(u8'Auto-Отправка', imgui.ImVec2(235, 165), true)
+	imgui.CenterText(u8"Настройки авто-отправки")
+	imgui.InputInt(u8'кд/сек', auto_wait)
+	imgui.SameLine()
+	imgui.TextQuestion('( ? )',u8"Время задержки в секундах")
+	imgui.NewInputText('##autotext', text_auto, 165, u8"Текст авто-отправки", 2)
+	imgui.SameLine()
+	local icon
+	if auto_send.v then icon = fa.ICON_FA_PAUSE_CIRCLE
+	elseif not auto_send.v then icon = fa.ICON_FA_PLAY end
+	if imgui.Button(icon .. '##1', imgui.ImVec2(20, 20)) then 
+		auto_send.v = not auto_send.v
+		if auto_send.v then
+			config.auto.text = u8:decode(text_auto.v)
+			config.auto.wait = auto_wait.v 
+			inicfg.save(config, directIni)
+			msgChat("Авто-Текст запущен")
+			msgChat("Задержка: ".. config.auto.wait .. " секунд")
+		elseif not auto_send.v then
+			msgChat("Авто-Текст завершен")
+		end
+		lua_thread.create(autosendfunc)
+	end
+	imgui.ToggleButton('Auto-Password', auto_password)
+	if auto_password.v then imgui.InputText('##password', password) end
+	imgui.SameLine()
+	if imgui.Button(fa.ICON_FA_PASTE .. '##savepassword', imgui.ImVec2(25, 20)) then
+		config.settings.password = password.v 
+		config.settings.auto_password = auto_password.v 
+		if inicfg.save(config,directIni) then
+			if bNotf then
+				notf.addNotification("Настройки авто-пароля сохранены", 3, 1)
+			end
+		end
+	end
+	imgui.EndChild()
+end
 function imgui.OnDrawFrame() -- Когда имгуи запущен
     local sw, sy = getScreenResolution() -- Позиция экрана
     if not main_window_state.v then imgui.Process = false end
@@ -189,78 +291,37 @@ function imgui.OnDrawFrame() -- Когда имгуи запущен
 		imgui.SetNextWindowSize(imgui.ImVec2(500, 200), imgui.Cond.FirstUseEver) 
         imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sy / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
 		imgui.Begin(u8" .SIXTEEN Настройки скрипта", main_window_state, imgui.WindowFlags.NoResize+imgui.WindowFlags.NoCollapse )
-			imgui.BeginChild('123', imgui.ImVec2(250, 165), true)
-			imgui.PushItemWidth(95)
-			imgui.InputText(u8'Ник кого скрыть', text_buffer)
+			FirstChild()
 			imgui.SameLine()
-			if imgui.Button(fa.ICON_FA_USER_PLUS .. '##1', imgui.ImVec2(40, 20)) then
-				if config.hide.nick ~= text_buffer.v then 
-					config.hide.nick = text_buffer.v
-					if inicfg.save(config, directIni) then msgChat("Ник сохранен") end
-				else msgChat("Этот ник и так в базе") end
-			end
-			imgui.ToggleButton(fa.ICON_FA_COMMENT .. u8' Прием сообщений', nvstatus)
-			imgui.SameLine()
-			imgui.TextQuestion('( ? )',u8"Когда чел пишет =[text], Ты отправишь [text] в чат")
-			if nvstatus.v then 
-				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ads', adsstatus)
-				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на /ask', askstatus)
-				imgui.Checkbox(fa.ICON_FA_ANGLE_DOUBLE_RIGHT ..u8' Реакция на !', gchatstatus)	
-			end
-			if imgui.Button(fa.ICON_FA_CLOUD_DOWNLOAD_ALT .. u8' Сохранить настройки', imgui.ImVec2(155, 30)) then
-				config.settings.nvstatus = nvstatus.v
-				config.settings.adsstatus = adsstatus.v
-				config.settings.askstatus = askstatus.v
-				config.settings.gchatstatus = gchatstatus.v
-				if inicfg.save(config, directIni) then msgChat("Настройки успешно сохранены") end
-			end
-			local x,y,z = getCharCoordinates(PLAYER_PED)
-			--imgui.Spinner(15,5)
-			imgui.PushItemWidth(120)
-			imgui.Combo(u8'', combo_dada, themes.colorThemes, #themes.colorThemes)
-			imgui.SameLine() 
-			if imgui.Button(fa.ICON_FA_PLAY .. "", imgui.ImVec2(50, 20)) then
-				themes.SwitchColorTheme(combo_dada.v +1)
-				config.settings.themeselected = combo_dada.v+1
-				if inicfg.save(config, directIni) then 
-					if bNotf then
-						notf.addNotification("Тема изменена! ", 3, 1)
-					end 
-				end
-			end
-			imgui.Text(u8"Координаты: " .. math.floor(x) .. " " .. math.floor(y) .. " " .. math.floor(z))
-			imgui.EndChild()
-			imgui.SameLine()
-			imgui.BeginChild(u8'Auto-Отправка', imgui.ImVec2(235, 165), true)
-			imgui.CenterText(u8"Настройки авто-отправки")
-			imgui.InputInt(u8'кд/сек', auto_wait)
-			imgui.SameLine()
-			imgui.TextQuestion('( ? )',u8"Время задержки в секундах")
-			imgui.NewInputText('##autotext', text_auto, 165, u8"Текст авто-отправки", 2)
-			imgui.SameLine()
-			if imgui.Button(fa.ICON_FA_PLAY .. '##1', imgui.ImVec2(20, 20)) then
-				auto_send.v = not auto_send.v
-				if auto_send.v then
-					config.auto.text = u8:decode(text_auto.v)
-					config.auto.wait = auto_wait.v 
-					inicfg.save(config, directIni)
-					msgChat("Авто-Текст запущен")
-					msgChat("Задержка: ".. config.auto.wait)
-				elseif not auto_send.v then
-					msgChat("Авто-Текст завершен")
-				end
-				lua_thread.create(autosendfunc)
-			end
-			imgui.EndChild()
+			SecondChild()
 		imgui.End()	
 	end
 end
 function autosendfunc()
-	if auto_send.v then
 		while auto_send.v do
-			wait(config.auto.wait*1000)
+			if auto_send.v then
 			sampSendChat(config.auto.text)
+			wait(config.auto.wait*1000)
+			elseif not auto_send.v then
+				break 
+			end
 		end 
+end
+function sampev.onApplyPlayerAnimation(playerId, animLib, animName, loop, lockX, lockY, freeze, time)
+	if checkanim then
+		msgChat("Библиотека: "..animLib)
+		msgChat("Название: "..animName)
+		msgChat("Фриз: "..tostring(freeze))
+		msgChat("Цикл: "..tostring(loop))
+	end
+end
+function sampev.onShowDialog(id, style, title, button1, button2, text)
+	if button1:match("Принимаю") and auto_password.v then
+		lua_thread.create(function()
+		sampSendDialogResponse(32700, 1, 1, nil)
+		wait(1)
+		sampSendDialogResponse(32700, 1, 0, password.v)
+		end)
 	end
 end
 function checkanim()
@@ -272,14 +333,6 @@ function checkanim()
 	end
 	config.settings.checkanim = checkanim
 	inicfg.save(config, directIni)
-end
-function sampev.onApplyPlayerAnimation(playerId, animLib, animName, loop, lockX, lockY, freeze, time)
-	if checkanim then
-		msgChat("Библиотека: "..animLib)
-		msgChat("Название: "..animName)
-		msgChat("Фриз: "..tostring(freeze))
-		msgChat("Цикл: "..tostring(loop))
-	end
 end
 function sampev.onServerMessage(color, text)
 	if text:find(config.hide.nick) then 
